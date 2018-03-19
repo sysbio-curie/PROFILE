6f8b4f5dbeca1577b013cad183b7497d77d8fe51
JonasBeal <33457182+JonasBeal@users.noreply.github.com>
2018-03-19 12:37:44 +0100

    Quiet requireNamespace

--- Scripts/Simulations/extract_output_probas.R
+++ Scripts/Simulations/extract_output_probas.R
@@ -12,7 +12,7 @@
 ## listOfConForNodes: Associated value to the conditions to analyze
 ## Example: values <- c(1,0,0) Value 1 for Node1, and Value 0 for Node2 and Node3
 
-if(!requireNamespace("optparse")){
+if(!requireNamespace("optparse", quietly = TRUE)){
 	install.packages("optparse")
 }
 
